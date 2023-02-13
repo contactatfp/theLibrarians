002c17637f3206df51fbc79e1fbc3716144cc2cf
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-02-13 12:31:51 -0800

    added index/about

--- dev/app.py
+++ dev/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
